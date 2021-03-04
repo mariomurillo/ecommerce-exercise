6ae133b532595502bd90466c3d324bb8d0b86da4
juliotobon <juceto@gmail.com>
2021-03-04 21:21:38 +0100

    Manejo de excepcion de error de conexion DB (#9)
    * Manejo de excepciones para la perdida de conexion con la DB
    
    * cambios en el manejo de excepciones
    
    * Feat: add configuration file necessary to enable GitHub Actions for our CI into the project
    
    * Feat: add postgres service into the CI configuration
    
    * Feat: remove unecessary files and add CI profile
    
    * Feat: add linters for java in CI configuration
    
    * Feat: disable copypaste jscpd linter of megalinters
    
    * Feat: add custom checks for java
    
    * Feat: update java checks to disable some unnecessary modules
    
    * Feat: join configuration files for CI
    
    * Feat: change position of steps in CI configuration
    
    * Feat: add step for checkout code in CI configuration
    
    * Se hicieron la correcciones a las observaciones hechas en el PR
    
    * Se hicieron las correciones de formato del codigo
    
    * Se corrigen los errores de formato y se agregan los modificadores final pedidos en la ci
    
    * Feat: disable SPELL_CSPELL of CI configuration
    
    * Feat: fix the disable in CI configuration
    
    * Feat: disable SPELL of CI configuration
    
    * Feat: add environment variable for DISABLE_LINTERS for the CI configuration
    
    Co-authored-by: Mario Murillo <mario.murillo@commure.com>
    Co-authored-by: mario.murillo <mardres@gmail.com>

---
+++ src/main/java/com/ecommerce/exceptions/AccountException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.exceptions;
+
+public class AccountException extends Exception {
+
+  public AccountException(final String message) {
+    super(message);
+  }
+}
--- src/main/java/com/ecommerce/service/AccountService.java
+++ src/main/java/com/ecommerce/service/AccountService.java
@@ -3,11 +3,12 @@ package com.ecommerce.service;
 import java.util.UUID;
 
 import com.ecommerce.domain.Account;
+import com.ecommerce.exceptions.AccountException;
 
 public interface AccountService {
 
   Account getAccountById(UUID id);
   void updateAccountById(Account account);
-  void createAccount(Account account);
+  void createAccount(Account account) throws AccountException;
   void deleteAccountById(UUID id);
 }
