160e5dfe1fc04d036d9e3475fb4cb90d2f6ad4c6
mario.murillo <mardres@gmail.com>
2021-03-04 15:35:53 -0500

    Add unit tests for create account (#11)
    * Manejo de excepciones para la perdida de conexion con la DB
    
    * cambios en el manejo de excepciones
    
    * Feat: Add unit test for the creation test and update the README file
    
    * Feat: Add unit test for the creation test and update the README file
    
    * Feat: add configuration file necessary to enable GitHub Actions for our CI into the project
    
    * Feat: add postgres service into the CI configuration
    
    * Feat: remove unecessary files and add CI profile
    
    * Feat: add linters for java in CI configuration
    
    * Feat: disable copypaste jscpd linter of megalinters
    
    * Feat: add custom checks for java
    
    * Feat: update java checks to disable some unnecessary modules
    
    * Feat: join configuration files for CI
    
    * Feat: change position of steps in CI configuration
    
    * Feat: add step for checkout code in CI configuration
    
    * Feat: apply format to solve linters issues
    
    * Feat: remove trailing spaces
    
    * Se hicieron la correcciones a las observaciones hechas en el PR
    
    * Se hicieron las correciones de formato del codigo
    
    * Se corrigen los errores de formato y se agregan los modificadores final pedidos en la ci
    
    * Feat: disable SPELL_CSPELL of CI configuration
    
    * Feat: fix the disable in CI configuration
    
    * Feat: disable SPELL of CI configuration
    
    * Feat: add environment variable for DISABLE_LINTERS for the CI configuration
    
    * Feat: update message exception in unit test for create account
    
    * Feat: update test to replace ConnectionException by AccountException
    
    Co-authored-by: julio cesar tobon <juceto@gmail.com>

---
+++ src/test/java/com/ecommerce/service/AccountServiceDefaultTest.java
@@ -0,0 +1,83 @@
+package com.ecommerce.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+import com.ecommerce.domain.Account;
+import com.ecommerce.domain.AccountType;
+import com.ecommerce.exceptions.AccountException;
+import com.ecommerce.repository.AccountRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class AccountServiceDefaultTest {
+
+  private AccountService service;
+  private AccountRepository repository;
+
+  @BeforeEach
+  public void setUp() {
+    repository = mock(AccountRepository.class);
+    service = new AccountServiceDefault(repository);
+  }
+
+  /**
+   * This method tests the account creation successfully
+   * */
+  @Test
+  public void createAccountSuccessfulTest() {
+    // The method `when` of Mockito together with the method `thenReturn` return
+    // something that we put into the parameter in `thenReturn` after to
+    // invoke the method `when`
+    // The `any` method return an any objetc
+    Mockito.when(repository.save(any())).thenReturn(any());
+
+    Account account = Account.builder()
+      .withEmail("test@email.com")
+      .withType(AccountType.ADMIN)
+      .build();
+
+    try {
+      service.createAccount(account);
+    } catch (AccountException ce) {
+      // If the `createAccount` method return an Exception the test fail
+      fail();
+    }
+
+    // The `verify` method verify that a method is called the quantity that
+    // is in the parameter in the `times` method
+    Mockito.verify(repository, times(1)).save(any());
+  }
+
+  /**
+   * This method tests the account creation with an Exception during the process
+   * */
+  @Test
+  public void createAccountWithExceptionTest() {
+    // We use the method `thenThrow` of Mockito to simulate the throw of an
+    // Exception when we call the method save
+    Mockito.when(repository.save(any())).thenThrow(new RuntimeException());
+
+    Account account = Account.builder()
+      .withEmail("test@email.com")
+      .withType(AccountType.ADMIN)
+      .build();
+
+    try {
+      service.createAccount(account);
+    } catch (AccountException ce) {
+      // We hope the `createAccount` throw an Exception and that exception that
+      // the same message that we put into the `assertEquals` method.
+      assertEquals(
+          "Can´t create account at this time. Please try again later",
+          ce.getMessage());
+    }
+
+    Mockito.verify(repository, times(1)).save(any());
+  }
+}
